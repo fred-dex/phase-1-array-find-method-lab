d3c386a86c3f2d00aedba20e9575397d6cd6a95b
freddex <fredjuiceman@yahoo.com>
2022-05-18 18:45:00 -0400

    commit tree bb9525b1ba793dc91b5a1166bd834575809147d4

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+const record = []
+superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W");
+    return !!result ? result.year : undefined;
+}
